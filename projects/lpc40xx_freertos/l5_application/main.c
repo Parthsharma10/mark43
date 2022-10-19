5e88259d2f6813dec9d4eca3f26d2ced78dafd89
Parth Sharma <parth100011@gmail.com>
2022-10-19 12:29:23 -0700

    Int

--- projects/lpc40xx_freertos/l5_application/main.c
+++ projects/lpc40xx_freertos/l5_application/main.c
@@ -1,85 +1,18 @@
 #include <stdio.h>
 
 #include "FreeRTOS.h"
+#include "task.h"
+
 #include "board_io.h"
 #include "common_macros.h"
-#include "esp32_task.h"
-
-#include "gpio.h"
-#include "gpio_lab.h"
-#include "lpc40xx.h"
-#include "lpc_peripherals.h"
 #include "periodic_scheduler.h"
-#include "semphr.h"
 #include "sj2_cli.h"
-#include "task.h"
 
 // 'static' to make these functions 'private' to this file
 static void create_blinky_tasks(void);
 static void create_uart_task(void);
 static void blink_task(void *params);
 static void uart_task(void *params);
-static void task_one(void *params);
-static void task_two(void *params);
-/*
-typedef struct {
-  float f1; // 4 bytes
-  char c1;  // 1 byte
-  float f2;
-  char c2;
-} __attribute__((packed)) my_s;
-*/
-// static SemaphoreHandle_t press_switch;
-static SemaphoreHandle_t button_semaphore = NULL;
-
-typedef struct {
-  /* First get gpio0 driver to work only, and if you finish it
-   * you can do the extra credit to also make it work for other Ports
-   */
-  uint8_t port;
-
-  uint8_t pin_num;
-
-} port_pin_s;
-
-void button_isr(void *task_parameter) {
-  const port_pin_s *button = (port_pin_s *)(task_parameter);
-  gpio0__set_as_input(button->pin_num);
-  long yield = 0;
-  while (1) {
-    if (gpio0__get_level(button->pin_num)) {
-      xSemaphoreGiveFromISR(button_semaphore, &yield);
-    }
-    portYIELD_FROM_ISR(yield);
-  }
-}
-/*static void button_task(void *task_parameter) {
-
-  const port_pin_s *button = (port_pin_s *)(task_parameter);
-  gpio0__set_as_input(button->pin_num);
-  {
-    while (1) {
-      if (gpio0__get_level(button->pin_num))
-        xSemaphoreGive(press_switch);
-    }
-    vTaskDelay(10);
-  }
-};
-*/
-void led_task(void *task_parameter) {
-  // Type-cast the paramter that was passed from xTaskCreate()
-  const port_pin_s *led = (port_pin_s *)(task_parameter);
-
-  while (true) {
-    if (xSemaphoreTake(button_semaphore, 5000)) {
-      gpio__set_low(led->pin_num, led->port);
-    } else {
-      gpio__set_high(led->pin_num, led->port);
-    }
-  }
-}
-
-// Step 2:
 
 void my_gpio_isr(void) {
   // a) Clear Port0/2 interrupt using CLR0 or CLR2 registers
@@ -87,16 +20,19 @@ void my_gpio_isr(void) {
 
   // b) Use fprintf(stderr) or blink and LED here to test your ISR
   fprintf(stderr, "In ISR ");
-  gpio__set_high(24, 1);
+  gpio_s gpio2 = {.port_number = 1, .pin_number = 24};
+  gpio__set(gpio2);
 }
 
 int main(void) {
+  create_blinky_tasks();
+  create_uart_task();
 
   // Read Table 95 in the LPC user manual and setup an interrupt on a switch connected to Port0 or Port2
   // a) For example, choose SW2 (P0_30) pin on SJ2 board and configure as input
   //.   Warning: P0.30, and P0.31 require pull-down resistors
-
-  gpio0__set_as_input(29);
+  gpio_s gpio1 = {.port_number = 0, .pin_number = 29};
+  gpio__set_as_input(gpio1);
 
   gpio_s gpio = {.port_number = 1, .pin_number = 18};
   gpio__set_as_output(gpio);
@@ -120,57 +56,17 @@ int main(void) {
     fprintf(stderr, "Main ");
   }
 
-  create_uart_task();
-
   // If you have the ESP32 wifi module soldered on the board, you can try uncommenting this code
   // See esp32/README.md for more details
   // uart3_init();                                                                     // Also include:  uart3_init.h
   // xTaskCreate(esp32_tcp_hello_world_task, "uart3", 1000, NULL, PRIORITY_LOW, NULL); // Include esp32_task.h
-  // printf("hello world\n");
+
   // puts("Starting RTOS");
-  // press_switch = xSemaphoreCreateBinary();
-  // button_semaphore = xSemaphoreCreateBinary();
-  // static port_pin_s button = {.pin_num = 29};
-
-  // static port_pin_s led = {.port = 1, .pin_num = 24};
-
-  // // xTaskCreate(button_task, "button", 2048 / sizeof(void *), &button, PRIORITY_LOW, NULL);
-  // xTaskCreate(led_task, "led1", 2048 / sizeof(void *), &led, PRIORITY_LOW, NULL);
-  // /*
-  // my_s s = {
-  //     .c1 = "tsla",
-  //     .c2 = "tsla",
-  //     .f1 = 0.1,
-  //     .f2 = 1.1,
-
-  // };
-  //* /
-  /*
-    printf("Size of structure : %d bytes\n"
-           "floats 0x%p 0x%p\n"
-           "chars  0x%p 0x%p\n",
-           sizeof(s), sizeof(s.f1, s.f2), sizeof(s.c1, s.c2));
-  */
   // vTaskStartScheduler(); // This function never returns unless RTOS scheduler runs out of memory and fails
+
   return 0;
 }
-/*
-static void task_one(void *task_parameter) {
 
-  while (true) {
-    fprintf(stderr, "AAAAAAAAAAAA");
-    vTaskDelay(100);
-  }
-}
-
-static void task_two(void *task_parameter) {
-
-  while (true) {
-    fprintf(stderr, "BBBBBBBBBBBB \n");
-    vTaskDelay(100);
-  }
-}
-*/
 static void create_blinky_tasks(void) {
   /**
    * Use '#if (1)' if you wish to observe how two tasks can blink LEDs
